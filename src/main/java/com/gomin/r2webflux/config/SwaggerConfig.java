dce9c4c7c1a703c39b1312e7808d5547e9dab1a2
김종수 <gomin@icanman.co.kr>
2025-02-02 14:16:59 +0900

    Swagger Change

--- src/main/java/com/gomin/r2webflux/config/SwaggerConfig.java
+++ src/main/java/com/gomin/r2webflux/config/SwaggerConfig.java
@@ -6,16 +6,24 @@ import io.swagger.v3.oas.models.info.Contact;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class SwaggerConfig {
+    
+    @Value("${springdoc.swagger-ui.theme:newspaper}")
+    private String theme;
     
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("R2DBC Account API")
-                        .description("Reactive Account Management API Documentation")
+                        .description("""
+                            Reactive Account Management API Documentation
+                            
+                            Theme: %s
+                            """.formatted(theme))
                         .version("v1.0")
                         .contact(new Contact()
                                 .name("Gomin")
